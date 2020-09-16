9200421cf3c2c1a19aa533eb2c59a6caf28361cc
Tilson Beattie <BeatTils377@GoTVDSB.ca>
2020-09-16 13:06:37 -0400

    Add project files.

---
+++ windows forms part 3/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_forms_part_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
